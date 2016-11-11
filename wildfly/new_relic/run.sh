fb326bf85e6e7e4a66fc627a8a9d46f7482c9a15
thikade <thomas.hikade@gmail.com>
2016-11-11 10:05:03 +0100

    PetStore and BackendSimulator app with JMeter stress test

---
+++ wildfly/new_relic/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker build -t thikade/wf821:newrelic-0.2  --build-arg LICENSE_KEY=e0bc4c990ecabe873dc5ebdb09b8ef28aabd6c62 .
+if [  $? -eq 0 ]; then
+  docker run  -ti --rm --name wf -v /c/Users/ThomasHikade/DockerVolumes/wildfly:/opt/jboss/wildfly/standalone/log -p 8080:8080 -p 9990:9990 -h wf821-newrelic thikade/wf821:newrelic-0.2
+else
+  echo "**** ERROR during docker build step!"
+fi

f8698aea2c15664e1cdbba9c354f488c1fac19c1
thikade <thomas.hikade@gmail.com>
2018-07-20 12:39:05 +0200

    scripts now tested with WAS85

---
+++ wasnd/nd/runme_was85_complete.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+WEB_URL=http://192.168.99.100:8080
+
+docker images | grep was85-noprofile
+if [ $? -ne 0 ] ; then
+
+    if [ ! -f "install/was.tar" ]; then
+      # cd ~/Documents/GitHub/dockerfiles/wasnd/network-deployment/
+      if docker ps | grep caddy ; then
+          echo "caddy is running"
+      else
+          echo "starting caddy"
+          docker run -d  --name caddy -p 8080:8080 -v /INSTALL_E:/data caddy
+      fi
+
+      curl -sSo /dev/null  $WEB_URL/WASND
+      if [ $? -eq 0 ] ; then
+        echo "caddy is working"
+      else
+        echo "caddy not working. Aborting"!
+        exit 100
+      fi
+
+      echo -e "\ninstalling WASND v85 - Step 1\n"
+      docker-compose -f docker-compose-build-was85.yml build wasnd_install_step1
+      docker images  | grep "was85_delete_me"
+      if [ $? -ne 0 ]; then
+         echo "ERROR: WAS intermediary install container not found!"
+         echo "==========="
+         docker images
+         echo "==========="
+         exit 101
+      fi
+
+      echo -e "\n\n*** creating was.tar (installing WASND v85 - Step 2)\n"
+      docker run -ti --rm -v $(pwd):/tmp was85_delete_me
+      if [ $? -ne 0 ]; then
+         echo "Error: failed in step2"
+         exit 102
+      fi
+
+      if [ -f "was.tar" ] ; then
+        echo -e "*** WAS Install image 'was.tar' created successfully"
+        docker rmi was85_delete_me
+        mv was.tar install/
+      else
+        exit 103
+      fi
+
+    fi
+
+    echo -e "\n\n*** creating the final WAS v85 Docker base image ...\n"
+    docker-compose -f docker-compose-build-was85.yml build wasnd_install_step2
+    docker images | grep was85-noprofile
+    if [ $? -eq 0 ] ; then
+      echo "WASv85 base image created successfully!"
+      echo "removing install/was.tar"
+      rm install/was.tar
+    else
+      exit 104
+    fi
+fi
+
+# wait for node rename operation upon first startup of nodes
+sleeptime=10
+maxwait=300
+
+
+
+docker images | grep wasnd85-dmgr || docker-compose -f docker-compose-build-was85.yml build dmgr
+docker-compose -f docker-compose-build-was85.yml up -d dmgr
+
+i=0
+while (( ($i * $sleeptime) < $maxwait )); do
+    sleep $sleeptime
+    let i=i+1
+    docker-compose -f docker-compose-build-was85.yml logs dmgr | grep 'DMGR SETUP COMPLETE'
+    rc=$?
+    test $rc -eq 0 && break
+done
+if [ $rc -gt 0 ]; then
+  echo "\n ** err during dmgr startup"
+  exit 99
+fi
+echo -e "\n*** Container dmgr is started\n"
+
+docker images |  grep wasnd85-node01 || docker-compose -f docker-compose-build-was85.yml build node01
+docker-compose -f docker-compose-build-was85.yml up -d node01
+
+i=0
+while (( ($i * $sleeptime) < $maxwait )); do
+    sleep $sleeptime
+    let i=i+1
+    docker-compose -f docker-compose-build-was85.yml logs node01 | egrep 'NODE RECONFIG COMPLETE'
+    rc=$?
+    test $rc -eq 0 && break
+done
+if [ $rc -gt 0 ]; then
+  echo "\n ** err during node01 initial startup"
+  exit 98
+fi
+
+
+echo -e "\n*** Container node01 is started\n"
+
+docker images |  grep wasnd85-node02 || docker-compose -f docker-compose-build-was85.yml build node02
+docker-compose -f docker-compose-build-was85.yml up -d node02
+
+i=0
+while (( ($i * $sleeptime) < $maxwait )); do
+    sleep $sleeptime
+    let i=i+1
+    docker-compose -f docker-compose-build-was85.yml logs node02 | egrep 'NODE RECONFIG COMPLETE'
+    rc=$?
+    test $rc -eq 0 && break
+done
+if [ $rc -gt 0 ]; then
+  echo "\n ** err during node02 initial startup"
+  exit 98
+fi
+
+echo -e "\n*** Container node02 is started\n"
+
+
+docker commit dmgr8 wasnd85-cell-dmgr
+docker commit node801 wasnd85-cell-node01
+docker commit node802 wasnd85-cell-node02
--- wasnd/nd/runme_was9_complete.sh
+++ wasnd/nd/runme_was9_complete.sh
@@ -5,7 +5,7 @@ WEB_URL=http://192.168.99.100:8080
 docker images | grep was9-noprofile
 if [ $? -ne 0 ] ; then
 
-    if [ ! -f "install/was.tar" ]; then
+    if [ ! -f "install/was9.tar" ]; then
       # cd ~/Documents/GitHub/dockerfiles/wasnd/network-deployment/
       if docker ps | grep caddy ; then
           echo "caddy is running"
@@ -23,7 +23,7 @@ if [ $? -ne 0 ] ; then
       fi
 
       echo "installing WASND v9 - Step 1"
-      docker-compose -f docker-compose-build_was9.yml build wasnd_install_step1
+      docker-compose -f docker-compose-build-was9.yml build wasnd_install_step1
       docker images  | grep "was9_delete_me"
       if [ $? -ne 0 ]; then
          echo "ERROR: WAS intermediary install container not found!"
@@ -33,17 +33,17 @@ if [ $? -ne 0 ] ; then
          exit 101
       fi
 
-      echo "creating was.tar (installing WASND v9 - Step 2)"
+      echo "creating was9.tar (installing WASND v9 - Step 2)"
       docker run -ti --rm -v $(pwd):/tmp was9_delete_me
       if [ $? -ne 0 ]; then
          echo "Error: failed in step2"
          exit 102
       fi
 
-      if [ -f "was.tar" ] ; then
-        echo "WAS Install image 'was.tar' created successfully"
+      if [ -f "was9.tar" ] ; then
+        echo "WAS Install image 'was9.tar' created successfully"
         docker rmi was9_delete_me
-        mv was.tar install/
+        mv was9.tar install/
       else
         exit 103
       fi
@@ -52,28 +52,29 @@ if [ $? -ne 0 ] ; then
 
 
     echo "creating the final WAS v9 Docker base image ..."
-    docker-compose -f docker-compose-build_was9.yml build wasnd_install_step2
+    docker-compose -f docker-compose-build-was9.yml build wasnd_install_step2
     docker images | grep was9-noprofile
     if [ $? -eq 0 ] ; then
       echo "WASv9 base image created successfully!"
-      echo "removing install/was.tar"
-      rm install/was.tar
+      echo "removing install/was9.tar"
+      rm install/was9.tar
     else
       exit 104
     fi
 fi
 
 #
-docker images | grep wasnd9-dmgr || docker-compose -f docker-compose-build_was9.yml build dmgr
-docker-compose -f docker-compose-build_was9.yml run -d dmgr
+docker images | grep wasnd9-dmgr || docker-compose -f docker-compose-build-was9.yml build dmgr
+docker-compose -f docker-compose-build-was9.yml up -d dmgr
 sleep 100
 echo "Container dmgr is started"
 
-# docker images | grep wasnd9-node01 || docker-compose -f docker-compose-build_was9.yml build node01
-docker-compose -f docker-compose-build_was9.yml run -d node01
+docker images | grep wasnd9-node01 || docker-compose -f docker-compose-build-was9.yml build node01
+docker-compose -f docker-compose-build-was9.yml up -d node01
 sleep 120
 echo "Container node01 is started"
 
-docker-compose -f docker-compose-build_was9.yml run -d node02
+docker images | grep wasnd9-node02 || docker-compose -f docker-compose-build-was9.yml build node02
+docker-compose -f docker-compose-build-was9.yml up -d node02
 sleep 120
 echo "Container node02 is started"

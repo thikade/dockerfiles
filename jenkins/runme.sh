a9d7e6abf8f321ce1c07d905f65df485cf6af434
thikade <thomas.hikade@gmail.com>
2019-05-29 17:48:41 +0200

    this version runs on minishift

--- jenkins/runme.sh
+++ jenkins/runme.sh
@@ -4,4 +4,11 @@ oc new-project custom-jenkins || oc project custom-jenkins
 
 oc create is jenkins-blueocean
 oc create is jenkins-blueocean-plugins
-oc new-build 
+
+
+# oc import-image openshift/jenkins-2-centos7:v3.11 --confirm
+
+oc new-build openshift/jenkins-2-centos7:v3.11~https://github.com/thikade/dockerfiles.git --name=jenkins-plugins --strategy=source  --context-dir=jenkins
+oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins --strategy=docker  --context-dir=jenkins 
+
+oc start-build jenkins-plugins

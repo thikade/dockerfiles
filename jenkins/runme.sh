43fc57fe842ad484079c1faf2b44a4ec3991980e
thikade <thomas.hikade@gmail.com>
2019-05-29 18:07:32 +0200

    fix volume name

--- jenkins/runme.sh
+++ jenkins/runme.sh
@@ -8,7 +8,10 @@ oc create is jenkins-blueocean-plugins
 
 # oc import-image openshift/jenkins-2-centos7:v3.11 --confirm
 
+# use 2 s2i build to install update plugins
 oc new-build openshift/jenkins-2-centos7:v3.11~https://github.com/thikade/dockerfiles.git --name=jenkins-plugins --strategy=source  --context-dir=jenkins
-oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins --strategy=docker  --context-dir=jenkins 
+
+# use docker build to build our own jenkins image
+oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins --strategy=docker  --context-dir=jenkins  --image-stream=jenkins-plugins:latest
 
 oc start-build jenkins-plugins

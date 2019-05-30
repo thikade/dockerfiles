dfc143858188f2d0628751f63373d40bc11b7de0
thikade <thomas.hikade@gmail.com>
2019-05-30 22:52:47 +0200

    updated parameters

--- jenkins-mdf/runme.sh
+++ jenkins-mdf/runme.sh
@@ -9,7 +9,7 @@ oc new-project custom-jenkins || oc project custom-jenkins
 oc new-build openshift/jenkins-2-centos7:v3.11~https://github.com/thikade/dockerfiles.git --name=jenkins-plugins --strategy=source  --context-dir=jenkins-mdf
 
 # use docker build to build our own jenkins image
-oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins --strategy=docker  --context-dir=jenkins-mdf  --image-stream=jenkins-plugins:latest  --allow-missing-imagestream-tags
+oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins -l app=jenkins --strategy=docker  --context-dir=jenkins-mdf  --image-stream=jenkins-plugins:latest  --allow-missing-imagestream-tags
 
 # process template and run jenkins
-oc process -f jenkins-template.yml | oc -n custom-jenkins create -f -
+oc process -f jenkins-template.yml -p JENKINS_SERVICE_NAME=jenkins -p JENKINS_VOLUME_NAME=pv0001 | oc -n custom-jenkins create -f -

a43758b1a3db36a79d5b7b0ae6f7a35a71a9c238
thikade <thomas.hikade@gmail.com>
2019-06-01 19:53:51 +0200

    new version

--- jenkins2/runme.sh
+++ jenkins2/runme.sh
@@ -1,21 +1,37 @@
 #!/bin/sh
 
 PROJECT=custom-jenkins-v2
-oc new-project $PROJECT || oc project $PROJECT
+oc delete project $PROJECT
+oc new-project $PROJECT
 
-  # oc import-image openshift/jenkins-2-centos7:v3.11 --confirm
-  # oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins --strategy=docker  --image-stream=jenkins-2-centos7:v3.11  --context-dir=jenkins2
+# oc import-image openshift/jenkins-2-centos7:v3.11 --confirm
 
 # use docker build to build our own jenkins image
-oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins-v2 -l app=jenkins-v2 --strategy=docker  --docker-image=docker.io/openshift/jenkins-2-centos7:v3.11  --context-dir=jenkins2
+oc new-build https://github.com/thikade/dockerfiles.git --name=jenkins -l app=jenkins --strategy=docker  --docker-image=docker.io/openshift/jenkins-2-centos7:v3.11  --context-dir=jenkins2
+
+# create configmap for XML configurations that will be mounted into container later-on
+oc create configmap jenkins-config-maven --from-file=configuration/hudson.tasks.Maven.xml --dry-run -o yaml | oc -n $PROJECT apply -f -
 
 # process template and run jenkins
-oc process -f jenkins-template.yml -p NAMESPACE=$PROJECT -p JENKINS_IMAGE_STREAM_TAG=jenkins-v2 -p JENKINS_SERVICE_NAME=jenkins-v2 -p JENKINS_VOLUME_NAME=pv0002 | oc create -f -
+### oc delete all -l template=jenkins-digi-template
+### oc get all -l template=jenkins-digi-template
 
-oc set env  dc/jenkins-v2  INSTALL_PLUGINS="nvm-wrapper"
-oc set env --list dc/jenkins-v2
+oc process -f
+ jenkins-template.yml -p NAMESPACE=$PROJECT -p JENKINS_IMAGE_STREAM_TAG=jenkins:latest -p JENKINS_SERVICE_NAME=jenkins -p JENKINS_VOLUME_NAME=pv0002 | oc apply -f -
 
-PODNAME=$(oc get pod -o name -l name=jenkins-v2)
+oc set triggers dc/jenkins --manual
+
+### configure JENKINS
+
+oc set volume dc/jenkins --add  --name=maven-config --mount-path=/var/lib/jenkins/hudson.tasks.Maven.xml --sub-path=hudson.tasks.Maven.xml  --type=configmap --configmap-name=jenkins-config-maven
+
+oc set env  dc/jenkins  INSTALL_PLUGINS="nvm-wrapper"
+# oc set env --list dc/jenkins
+
+oc set triggers dc/jenkins --auto
+
+PODNAME=$(oc get pod -o name -l name=jenkins)
+oc logs -f $PODNAME
 oc logs $PODNAME | grep plugin
 
 oc create -f pipeline_buildconfig.yml

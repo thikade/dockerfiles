5372bca3f127304cef8f488db4651abc2e9e08fd
thikade <thomas.hikade@gmail.com>
2019-09-17 09:59:56 +0200

    add jmx_exporter & upgrade to WAS 9.0.5.0

--- wasnd/nd/runme_was9_complete.sh
+++ wasnd/nd/runme_was9_complete.sh
@@ -2,86 +2,122 @@
 
 WEB_URL=${1:-http://192.168.99.100:8080}
 
-docker images | grep wasnd9-noprofile
-if [ $? -ne 0 ] ; then
+IMAGE_VERSION=9050
+BASE_IMAGE=wasnd9-noprofile:9050
 
-    if [ ! -f "install/was9.tar" ]; then
-      if docker ps | grep caddy ; then
-          echo "caddy is running"
+echo "#1 checking for base image: \"${BASE_IMAGE}\""
+
+docker images --format ' {{.Repository }}:{{.Tag}}' | grep ${BASE_IMAGE}  > /dev/null
+if [ $? -ne 0 ] ; then
+    echo "#1.1 building base image \"${BASE_IMAGE}\""
+      echo "wait..."
+      read key
+    echo -e "#2 Checking for WAS.tar export file ..."
+    if [ ! -f "was9.tar" ]; then
+      echo -e "#2.1 install/was9.tar not found. Installing base WAS."
+      if docker ps | grep caddy  > /dev/null ; then
+          echo "#2.2 caddy is running"
       else
-          echo "starting caddy"
+          echo "#2.2 starting caddy"
           docker run -d  --name caddy -p 8080:8080 -v /INSTALL:/data caddy
       fi
 
       curl -sSo /dev/null  $WEB_URL/WASND
       if [ $? -eq 0 ] ; then
-        echo "caddy is working"
+        echo "#2.3 caddy tested ok"
       else
-        echo "caddy not working. Aborting"!
+        echo "#2.3 caddy reported 404. Aborting"!
         exit 100
       fi
 
-      echo "installing WASND v9 - Step 1"
+      echo "#2.4 installing WASND v9 - Step 1"
       docker-compose -f docker-compose-build-was9.yml build wasnd_install_step1
-      docker images  | grep "was9_delete_me"
+      docker images --format ' {{.Repository }}:{{.Tag}}'  | grep "was9_delete_me" > /dev/null
       if [ $? -ne 0 ]; then
-         echo "ERROR: WAS intermediary install container not found!"
+         echo "#2.4 ERROR: WAS intermediary install container not found!"
          echo "==========="
          docker images
          echo "==========="
          exit 101
       fi
 
-      echo "creating was9.tar (installing WASND v9 - Step 2)"
+      echo "#2.5 creating was9.tar (installing WASND v9 - Step 2)"
       docker run -ti --rm -v $(pwd):/tmp was9_delete_me
       if [ $? -ne 0 ]; then
-         echo "Error: failed in step2"
+         echo "#2.5 Error: creating was9.tar failed"
          exit 102
       fi
 
       if [ -f "was9.tar" ] ; then
-        echo "WAS Install image 'was9.tar' created successfully"
+        echo "#2.6 WAS Install image 'was9.tar' created successfully"
         docker rmi was9_delete_me
-        mv was9.tar install/
+        ln was9.tar install/
       else
         exit 103
       fi
 
+    else
+      echo -e "#2 was9.tar already there. Skipping build."
+      # check symlink
+      test ! -f install/was9.tar && ln  was9.tar install/
     fi
 
 
-    echo "creating the final WAS v9 Docker base image ..."
+    echo "#2.10 creating the final WAS v9 Docker base image using tar ..."
     docker-compose -f docker-compose-build-was9.yml build wasnd_install_step2
-    docker images | grep wasnd9-noprofile
+    docker images --format ' {{.Repository }}:{{.Tag}}' | grep ${BASE_IMAGE}  > /dev/null
     if [ $? -eq 0 ] ; then
-      echo "WASv9 base image created successfully!"
-      echo "removing install/was9.tar"
-      rm install/was9.tar
+      echo "#2.10 WASv9 base image \"${BASE_IMAGE}\" created successfully!"
+      echo "#2.10 DEBUG: NOT removing install/was9.tar"
+      # rm install/was9.tar was9.tar
     else
+      # failed
       exit 104
     fi
+else
+  echo "#1 already up-to-date base image \"${BASE_IMAGE}\" found"
 fi
 
 #
-docker images | grep wasnd9-dmgr || docker-compose -f docker-compose-build-was9.yml build dmgr
+echo "#3 creating a working cell (dmgr + 2 nodes) ..."
+docker images --format ' {{.Repository }}:{{.Tag}}' | grep wasnd9-dmgr:$IMAGE_VERSION  > /dev/null
+if [ $? -ne 0 ] ; then
+  echo "#3 Bootstrapping DMGR ..."
+  docker-compose -f docker-compose-build-was9.yml build dmgr
+fi
+echo "#3 Starting DMGR ..."
 docker-compose -f docker-compose-build-was9.yml up -d dmgr
 sleep 100
-echo "Container dmgr is started"
+echo "#3 Container dmgr is started"
 
-docker images | grep wasnd9-node01 || docker-compose -f docker-compose-build-was9.yml build node01
+exit
+
+docker images --format ' {{.Repository }}:{{.Tag}}' | grep wasnd9-node01:$IMAGE_VERSION  > /dev/null
+if [ $? -ne 0 ] ; then
+  echo "#4 Bootstrapping node01 ..."
+  docker-compose -f docker-compose-build-was9.yml build node01
+fi
+echo "#4 Starting Node01 ..."
 docker-compose -f docker-compose-build-was9.yml up -d node01
 sleep 120
-echo "Container node01 is started"
+echo "#4 Container node01 is started"
 
-docker images | grep wasnd9-node02 || docker-compose -f docker-compose-build-was9.yml build node02
+
+docker images --format ' {{.Repository }}:{{.Tag}}' | grep wasnd9-node02:$IMAGE_VERSION  > /dev/null
+if [ $? -ne 0 ] ; then
+  echo "#5 Bootstrapping node02 ..."
+  docker-compose -f docker-compose-build-was9.yml build node02
+fi
+echo "#5 Starting Node02 ..."
 docker-compose -f docker-compose-build-was9.yml up -d node02
 sleep 120
-echo "Container node02 is started"
+echo "#5 Container node01 is started"
 
 
-docker commit dmgr wasnd9-cell-dmgr
-docker commit node01 wasnd9-cell-node01
-docker commit node02 wasnd9-cell-node02
+echo "#6 - committing new cell images ..."
+docker commit dmgr wasnd9-cell-dmgr:$IMAGE_VERSION
+docker commit node01 wasnd9-cell-node01:$IMAGE_VERSION
+docker commit node02 wasnd9-cell-node02:$IMAGE_VERSION
 
-docker images| grep "wasnd9-cell"
+docker images --format ' {{.Repository }}:{{.Tag}}'| grep "wasnd9-cell"| grep $IMAGE_VERSION
 echo -e "\n*** WASv9 CELL images commited. All operations finished!\n"

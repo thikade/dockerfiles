d417e948987e99529a0b807f257f7651cb99d3f8
thikade <thomas.hikade@gmail.com>
2019-09-17 13:54:08 +0200

    new chained build to improve perf/complexity

--- wasnd/nd/runme_was9_complete.sh
+++ wasnd/nd/runme_was9_complete.sh
@@ -25,77 +25,40 @@ echo "#1 checking for base image: \"${BASE_IMAGE}\""
 docker images --format ' {{.Repository }}:{{.Tag}}' | grep ${BASE_IMAGE}  > /dev/null
 if [ $? -ne 0 ] ; then
     echo "#1.1 building base image \"${BASE_IMAGE}\""
-      echo "press any key to continue ..."
-      read key
-    echo -e "#2 Checking for WAS.tar export file ..."
-    if [ ! -f "was9.tar" ]; then
-      echo -e "#2.1 install/was9.tar not found. Installing base WAS."
-      if docker ps | grep caddy  > /dev/null ; then
-          echo "#2.2 caddy is running"
-      else
-          echo "#2.2 starting caddy"
-          docker run -d  --name caddy -p 8080:8080 -v /INSTALL:/data caddy
-      fi
+    sleep 2
 
-      curl -sSo /dev/null  $WEB_URL/WASND
-      if [ $? -eq 0 ] ; then
-        echo "#2.3 caddy tested ok"
-      else
-        echo "#2.3 caddy reported 404. Aborting"!
-        exit 100
-      fi
 
-      echo "#2.4 installing WASND v9 - Step 1"
-      docker-compose -f docker-compose-build-was9.yml build wasnd_install_step1
-      docker images --format ' {{.Repository }}:{{.Tag}}'  | grep "was9_delete_me" > /dev/null
-      if [ $? -ne 0 ]; then
-         echo "#2.4 ERROR: WAS intermediary install container not found!"
-         echo "==========="
-         docker images
-         echo "==========="
-         exit 101
-      fi
-
-      echo "#2.5 creating was9.tar (installing WASND v9 - Step 2)"
-      docker run -ti --rm -v $(pwd):/tmp was9_delete_me
-      if [ $? -ne 0 ]; then
-         echo "#2.5 Error: creating was9.tar failed"
-         exit 102
-      fi
-
-      if [ -f "was9.tar" ] ; then
-        echo "#2.6 WAS Install image 'was9.tar' created successfully"
-        docker rmi was9_delete_me
-        # ln was9.tar install/
-      else
-        exit 103
-      fi
-
+    if docker ps | grep caddy  > /dev/null ; then
+        echo "#2.2 caddy is running"
     else
-      echo -e "#2 was9.tar already there. Skipping build."
-      # check symlink
-      # test ! -f install/was9.tar && ln  was9.tar install/
+        echo "#2.2 starting caddy"
+        docker run -d  --name caddy -p 8080:8080 -v /INSTALL:/data caddy
     fi
 
-
-    echo "#2.10 creating the final WAS v9 Docker base image using tar ..."
-    docker-compose -f docker-compose-build-was9.yml build wasnd_install_step2
-    docker images --format ' {{.Repository }}:{{.Tag}}' | grep ${BASE_IMAGE}  > /dev/null
+    curl -sSo /dev/null  $WEB_URL/WASND
     if [ $? -eq 0 ] ; then
-      echo "#2.11 WASv9 base image \"${BASE_IMAGE}\" created successfully!"
-      echo "#2.12 DEBUG: NOT removing install/was9.tar"
-      # rm install/was9.tar was9.tar
-      # rm was9.tar
+      echo "#2.3 caddy tested ok"
     else
-      # failed
-      exit 104
+      echo "#2.3 caddy reported 404. Aborting"!
+      exit 100
     fi
+
+    echo "#2.4 installing WASND v9"
+    docker-compose -f docker-compose-build-was9.yml build wasnd_chained_build
+    docker images --format ' {{.Repository }}:{{.Tag}}'  | grep "$BASE_IMAGE" > /dev/null
+    if [ $? -ne 0 ]; then
+       echo "#2.4 ERROR: base image $BASE_IMAGE not found!"
+       echo "==========="
+       exit 101
+    fi
+
 else
   echo "#1 already up-to-date base image \"${BASE_IMAGE}\" found"
 fi
 
+
 #
-echo "#3 creating a working cell (dmgr + 2 nodes) ..."
+echo '#3 creating a working cell (dmgr + 2 nodes) ...'
 docker images --format ' {{.Repository }}:{{.Tag}}' | grep wasnd9-dmgr:$IMAGE_VERSION  > /dev/null
 if [ $? -ne 0 ] ; then
   echo "#3 Bootstrapping DMGR ..."

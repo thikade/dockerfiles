8e2bd7aec0e6d8af04290124508f92bf603b71f5
thikade <thomas.hikade@gmail.com>
2023-10-22 21:38:59 +0200

    inital release of grafana-config playbook for users, folders and teams

---
+++ python-ansible-grafana/build.sh
@@ -0,0 +1,2 @@
+docker build -t grafana-config:latest .
+docker run --rm  grafana-config
---
+++ python-ansible-grafana/playbook/run.sh
@@ -0,0 +1,7 @@
+ansible --version
+# while true
+# do
+    cowsay -t "$(date)"
+    ansible-playbook playbook/site.yaml
+    sleep 1
+# done
